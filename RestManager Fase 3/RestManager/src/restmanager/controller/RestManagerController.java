86c529f9e737c9deb015c9977e525657cad6781f
fernandopedridodev <fernandopedridodev@gmail.com>
2025-06-15 18:16:00 +0200

    Add files via upload

---
+++ RestManager Fase 3/RestManager/src/restmanager/controller/RestManagerController.java
@@ -0,0 +1,164 @@
+package restmanager.controller;
+
+import restmanager.db.OrderDB;
+import restmanager.model.MenuItem;
+import restmanager.model.Order;
+import restmanager.model.OrderItem;
+import restmanager.view.OrderManagerView;
+import restmanager.view.OrderView;
+import restmanager.view.RestManagerView;
+
+/**
+ * Clase que implementa o controlador que xestiona o fluxo de comportamento da
+ * aplicación
+ *
+ * @author Profe de Programación
+ */
+public class RestManagerController {
+
+    // Referenza á vista da app
+    private RestManagerView view = new RestManagerView(this);
+    // Referenza á vista de xestión dos pedidos
+    private OrderManagerView ordersView;
+
+    /**
+     * Método que inicia a aplicación, mostrando o menú de inicio ata que se
+     * queira saír
+     */
+    public void initApp() {
+        boolean exit;
+
+        do {
+            exit = view.showInitMenu();
+        } while (!exit);
+    }
+
+    /**
+     * Crea un novo pedido e o completa pedindo todos os pratos que se queiran
+     * engadir, ou engade pratos a un pedido existente
+     *
+     * @param tableNumber Número da mesa para a que queremos completar o pedido
+     */
+    public void completeOrder(int tableNumber) {
+        // Buscamos se hai un pedido para a mesa recibida
+        Order order = OrderDB.findByTable(tableNumber);
+        // Se non hai un pedido para a mesa, creámolo e gardámolo
+        if (order == null) {
+            order = new Order(tableNumber);
+            OrderDB.save(order);
+        }
+        // Pedimos elementos da carta para engadir ao pedido, ata que se indique
+        // que non se queren engadir máis elementos
+        int menuItemId;
+        do {
+            // Chamamos á vista para pedir un elemento
+            menuItemId = view.askOrderItem(order);
+            // Engadimos o elemento se o Id introducido non é 0
+            if (menuItemId != 0) {
+                // Buscamos un elemento co Id introducido
+                MenuItem item = OrderDB.findById(menuItemId);
+                // Se non hai un elemento co Id introducido, informamos do erro
+                if (item == null) {
+                    view.showItemNotFoundMessage();
+                } else {
+                    // Buscamos nos elementos do pedido se xa hai un elemento
+                    // asociado ao elemento da carta pedido, para nese caso
+                    // incrementar a cantidade dese elemento
+                    boolean menuItemFound = false;
+                    for (OrderItem orderItem : order.getItems()) {
+                        if (orderItem.getItem().getId() == item.getId()) {
+                            orderItem.setQuantity(orderItem.getQuantity() + 1);
+                            // Actualizamos os datos do elemento
+                            OrderDB.updateItem(order, orderItem);
+                            menuItemFound = true;
+                        }
+                    }
+                    // Se non hai elemento no pedido para o elemento da carta
+                    // pedido, engadimos un novo elemento no pedido con
+                    // cantidade 1
+                    if (!menuItemFound) {
+                        OrderItem orderItem = new OrderItem(item, 1);
+                        order.getItems().add(orderItem);
+                        // Gardamos os datos do elemento
+                        OrderDB.saveItem(order, orderItem);
+                    }
+                }
+            }
+        } while (menuItemId != 0);
+    }
+
+    /**
+     * Obtén os datos dun pedido para mostralo por pantalla
+     *
+     * @param tableNumber Número de mesa para o que queremos ver o pedido
+     */
+    public void showOrder(int tableNumber) {
+        Order order = OrderDB.findByTable(tableNumber);
+        view.showOrderItems(order);
+    }
+
+    /**
+     * Mostra todos os pedidos
+     */
+    public void showOrders() {
+        ordersView = new OrderManagerView(this);
+        ordersView.setOrders(OrderDB.getAllOrders());
+        ordersView.setVisible(true);
+    }
+
+    /**
+     * Actualiza a lista de todos os pedidos
+     */
+    public void refreshOrders() {
+        ordersView.setOrders(OrderDB.getAllOrders());
+    }
+
+    /**
+     * Mostra un diálogo cos datos dun pedido
+     *
+     * @param order Pedido que se quere mostrar
+     */
+    public void showOrderDetails(Order order) {
+        // Cargamos todos os datos do pedido
+        order = OrderDB.findByTable(order.getTableNumber());
+        // Creamos o cadro de diálogo e mostrámolo
+        OrderView orderView = new OrderView(ordersView, true, this);
+        orderView.setOrder(order);
+        orderView.setLocationRelativeTo(ordersView);
+        orderView.setVisible(true);
+        // Se se indicou a opción de cobrar o pedido eliminámolo
+        if (orderView.isOrderPaid()) {
+            OrderDB.removeOrder(order.getTableNumber());
+        }
+        // Actualizamos a lista de todos pedidos
+        ordersView.setOrders(OrderDB.getAllOrders());
+    }
+
+    /**
+     * Borra unha unidade dun elemento do pedido
+     *
+     * @param order Pedido do que se quere quitar a unidade
+     * @param index Índice do elemento do que se quere quitar a unidade
+     */
+    public void removeOrderItem(Order order, int index) {
+        OrderItem item = order.getItems().get(index);
+        if (item.getQuantity() == 1) {
+            OrderDB.removeOrderItem(order, item);
+        } else {
+            OrderDB.decreaseOrderItem(order, item);
+        }
+    }
+
+    /**
+     * Método inicial
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RestManagerController controller = new RestManagerController();
+        controller.initApp();
+        // Pechamos a conexión coa base de datos
+        OrderDB.closeConnection();
+    }
+
+}
---
+++ RestManager Fase 3/RestManager/src/restmanager/db/OrderDB.java
@@ -0,0 +1,292 @@
+package restmanager.db;
+
+import java.util.ArrayList;
+import restmanager.model.Order;
+import restmanager.model.MenuItem;
+import restmanager.model.OrderItem;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Clase que implementa a persistencia dos pedidos e os elementos da carta
+ *
+ * @author Profe de Programación
+ */
+public class OrderDB {
+
+    // Array cos elementos da carta
+    private static final MenuItem[] menu = new MenuItem[]{
+        new MenuItem(1, "Spaguetti Carbonara", 12.50),
+        new MenuItem(2, "Pizza catro queixos mediana", 11.90),
+        new MenuItem(3, "Cocacola 33 cl", 2.5),
+        new MenuItem(4, "Auga 1l", 3)
+    };
+
+    // Conexión coa base de datos
+    private static Connection c = null;
+
+    /**
+     * Método que establece a conexión coa base de datos se é necesario
+     *
+     * @throws SQLException
+     */
+    private static void getConnection() throws SQLException {
+        if (c == null) {
+            c = DriverManager.getConnection("jdbc:sqlite:rest.db");
+        }
+    }
+    
+    /**
+     * Método que pecha a conexión coa base de datos
+     */
+    public static void closeConnection() {
+        if (c != null) {
+            System.out.print("Pechando conexión coa base de datos...");
+            try {
+                c.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+            System.out.println("[OK]");
+        }
+    }
+
+    /**
+     * Obtén o elemento de menú cun Id determinado
+     *
+     * @param menuItemId Id do elemento de menú que buscamos
+     * @return Elemento de menú con ese Id, ou null se non hai un elemento co Id
+     * indicado
+     */
+    public static MenuItem findById(int menuItemId) {
+        for (MenuItem item : menu) {
+            if (item.getId() == menuItemId) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Obtén o pedido para un número de mesa determinado
+     *
+     * @param tableNumber Número de mesa para o que buscamos o pedido
+     * @return Pedido asociado á mesa, ou null se non hai un pedido asociado á
+     * mesa indicada
+     */
+    public static Order findByTable(int tableNumber) {
+        Order order = null;
+        try {
+            // Establecemos a conexión coa base de datos se é necesario
+            getConnection();
+            // Executamos a consulta que obtén o pedido
+            String sql = "SELECT * FROM orders WHERE table_number=?";
+            PreparedStatement pst = c.prepareStatement(sql);
+            pst.setInt(1, tableNumber);
+            ResultSet rst = pst.executeQuery();
+            // Xeramos o pedido resultante cos seus elementos
+            if (rst.next()) {
+                order = new Order(tableNumber);
+                // Executamos a consulta que obtén os elementos do pedido
+                String sqlItems = "SELECT * FROM order_items WHERE table_number=?";
+                PreparedStatement pstItems = c.prepareStatement(sqlItems);
+                pstItems.setInt(1, tableNumber);
+                ResultSet rstItems = pstItems.executeQuery();
+                while (rstItems.next()) {
+                    OrderItem item = new OrderItem(findById(rstItems.getInt("item_id")),
+                            rstItems.getInt("quantity"));
+                    order.getItems().add(item);
+                }
+                // Pechamos os recursos
+                rstItems.close();
+                pstItems.close();
+            }
+            // Pechamos os recursos
+            rst.close();
+            pst.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return order;
+    }
+
+    /**
+     * Almacena un novo pedido
+     *
+     * @param order Pedido que se quere gardar
+     */
+    public static void save(Order order) {
+        try {
+            // Establecemos a conexión coa base de datos se é necesario
+            getConnection();
+            // Executamos a consulta de inserción do pedido
+            String sql = "INSERT INTO orders VALUES(?)";
+            PreparedStatement pst = c.prepareStatement(sql);
+            pst.setInt(1, order.getTableNumber());
+            pst.executeUpdate();
+            // Pechamos os recursos
+            pst.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Almacena un novo elemento nun pedido
+     *
+     * @param order Pedido do elemento
+     * @param item Elemento do pedido que se quere gardar
+     */
+    public static void saveItem(Order order, OrderItem item) {
+        try {
+            // Establecemos a conexión coa base de datos se é necesario
+            getConnection();
+            // Executamos a consulta de inserción do pedido
+            String sql = "INSERT INTO order_items VALUES(?,?,?)";
+            PreparedStatement pst = c.prepareStatement(sql);
+            pst.setInt(1, order.getTableNumber());
+            pst.setInt(2, item.getItem().getId());
+            pst.setInt(3, item.getQuantity());
+            pst.executeUpdate();
+            // Pechamos os recursos
+            pst.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Actualiza un elemento nun pedido
+     *
+     * @param order Pedido do elemento
+     * @param item Elemento do pedido que se quere actualizar
+     */
+    public static void updateItem(Order order, OrderItem item) {
+        try {
+            // Establecemos a conexión coa base de datos se é necesario
+            getConnection();
+            // Executamos a consulta de inserción do pedido
+            String sql = "UPDATE order_items SET quantity=? WHERE "
+                    + "table_number=? AND item_id=?";
+            PreparedStatement pst = c.prepareStatement(sql);
+            pst.setInt(1, item.getQuantity());
+            pst.setInt(2, order.getTableNumber());
+            pst.setInt(3, item.getItem().getId());
+            pst.executeUpdate();
+            // Pechamos os recursos
+            pst.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Obtén a lista con todos os pedidos
+     *
+     * @return Lista con todos os pedidos
+     */
+    public static ArrayList<Order> getAllOrders() {
+        ArrayList<Order> orders = new ArrayList<>();
+        try {
+            // Establecemos a conexión coa base de datos se é necesario
+            getConnection();
+            // Executamos a consulta que obtén os elementos do pedido
+            String sql = "SELECT * FROM orders";
+            Statement st = c.createStatement();
+            ResultSet rst = st.executeQuery(sql);
+            // Xeramos o pedido resultante cos seus elementos
+            while (rst.next()) {
+                orders.add(new Order(rst.getInt("table_number")));
+            }
+            // Pechamos os recursos
+            rst.close();
+            st.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return orders;
+    }
+
+    /**
+     * Elimina un pedido
+     *
+     * @param tableNumber Mesa á que está asociada o pedido a borrar
+     */
+    public static void removeOrder(int tableNumber) {
+        try {
+            // Establecemos a conexión coa base de datos se é necesario
+            getConnection();
+            // Executamos a consulta de borrado dos elementos do pedido
+            String sql = "DELETE FROM order_items WHERE table_number=?";
+            PreparedStatement pst = c.prepareStatement(sql);
+            pst.setInt(1, tableNumber);
+            pst.executeUpdate();
+            // Executamos a consulta de borrado do pedido
+            sql = "DELETE FROM orders WHERE table_number=?";
+            pst = c.prepareStatement(sql);
+            pst.setInt(1, tableNumber);
+            pst.executeUpdate();
+            // Pechamos os recursos
+            pst.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Elimina un elemento dun pedido
+     *
+     * @param order Pedido ao que se lle quere eliminar o elemento
+     * @param item Elemento a eliminar
+     */
+    public static void removeOrderItem(Order order, OrderItem item) {
+        order.getItems().remove(item);
+        try {
+            // Establecemos a conexión coa base de datos se é necesario
+            getConnection();
+            // Executamos a consulta de borrado do elementos do pedido
+            String sql = "DELETE FROM order_items WHERE table_number=? AND "
+                    + "item_id=?";
+            PreparedStatement pst = c.prepareStatement(sql);
+            pst.setInt(1, order.getTableNumber());
+            pst.setInt(2, item.getItem().getId());
+            pst.executeUpdate();
+            // Pechamos os recursos
+            pst.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Decrementa unha unidade na cantidade dun elemento dun pedido
+     *
+     * @param order Pedido no que está o elemento
+     * @param item Elmento ao que se lle quere quitar unha unidade na cantidade
+     */
+    public static void decreaseOrderItem(Order order, OrderItem item) {
+        item.setQuantity(item.getQuantity() - 1);
+        try {
+            // Establecemos a conexión coa base de datos se é necesario
+            getConnection();
+            // Executamos a consulta de actualización do elemento do pedido
+            String sql = "UPDATE order_items SET quantity=? WHERE "
+                    + "table_number=? AND item_id=?";
+            PreparedStatement pst = c.prepareStatement(sql);
+            pst.setInt(1, item.getQuantity());
+            pst.setInt(2, order.getTableNumber());
+            pst.setInt(3, item.getItem().getId());
+            pst.executeUpdate();
+            // Pechamos os recursos
+            pst.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
